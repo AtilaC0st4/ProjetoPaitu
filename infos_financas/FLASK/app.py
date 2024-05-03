ec331fe424202ef3eec26cacd119d24223d8b918
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-03 09:22:40 -0300

    montando a estrutura do template

--- infos_financas/FLASK/app.py
+++ infos_financas/FLASK/app.py
@@ -4,12 +4,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
  
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/calculadora')
+def calculadora():
+    return render_template('calculadora.html') 
+
+# @app.route('/enviar')
+# def enviar():
+
+    #     variaveis de acordo com o forms para salvar 
+    
+    # return 
+
+
+ 
  
  
  
