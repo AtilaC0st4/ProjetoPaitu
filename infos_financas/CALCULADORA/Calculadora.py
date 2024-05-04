a46ffc1b9fe1cc175b41ed5daecfd9eea833a559
José <jrsneto.2005@gmail.com>
2024-05-04 11:09:07 -0300

    Treinando a Calculadora

--- infos_financas/CALCULADORA/Calculadora.py
+++ infos_financas/CALCULADORA/Calculadora.py
@@ -1,56 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
 
-SELIC = 12.75
-IPCA = 10.2
-CDI = SELIC - 0.1
-
-tax_equi_Selic = (1+SELIC/100)**(1/252) - 1
-tax_equi_Ipca = (1+IPCA/100)**(1/252) - 1
-tax_equi_Cdi = (1+CDI/100)**(1/252) - 1
-def invest_selic(montante, tempo):
+def invest_selic(montante, tempo, taxa):
+    taxa_equi_Selic = (1+taxa/100)**(1/252) - 1
     VF = 0
     for cont in range(tempo):
-        VF = (VF + montante)*(1+tax_equi_Selic)
+        VF = (VF + montante)*(1+taxa_equi_Selic)
     return VF
-teste1 = invest_selic((300*12/252),1260)
 
-print(f"SELIC: {teste1}") 
-
-def invest_cdi(montante, tempo):
+def invest_cdi(montante, tempo, taxa):
+    taxa_equi_Cdi = (1+taxa/100)**(1/252) - 1
     VF = 0
     for cont in range(tempo):
-        VF = (VF + montante) * (1+tax_equi_Cdi)
+        VF = (VF + montante) * (1+taxa_equi_Cdi)
     return VF
-teste2 = invest_cdi((300*12/252), 1260)
 
-print(f"CDI:{teste2}")
-
-def invest_ipca(montante, tempo):
+def invest_ipca(montante, tempo, taxa):
+    taxa_equi_Ipca = (1+taxa/100)**(1/252) - 1
     VF = 0
     for cont in range(tempo):
-        VF = (VF + montante) *(1+tax_equi_Ipca)
+        VF = (VF + montante) *(1+taxa_equi_Ipca)
     return VF
-teste3 = invest_ipca((3000*12/252), 1260)
 
-print(f"IPCA : {teste3}")
-
-def invest_porcentagem_cdi(montante, tempo, porcentagem):
+def invest_porcentagem_cdi(montante, tempo, taxa, porcentagem):
+    taxa_equi_Cdi = (1+taxa/100)**(1/252) - 1
     VF = 0
     for cont in range(tempo):
-        VF = (VF + montante) * (1+(porcentagem/100 * tax_equi_Cdi))
+        VF = (VF + montante) * (1+(porcentagem/100 * taxa_equi_Cdi))
     return VF
-teste4 = invest_porcentagem_cdi((3000*12/252), 1260, 110)
 
-print(f"Porcentagem %CDI: {teste4}")
-
-def invest_Ipca_mais(montante, tempo, mais):
-    VF = 0
+def invest_Ipca_mais(montante, tempo, taxa, mais):
+    taxa_equi_Ipca = (1+taxa/100)**(1/252) - 1
     taxa_equivalente_Ipca = (1+mais/100) **(1/252) - 1
+    VF = 0
     for cont in range(tempo):
-        VF = (VF + montante) * ((1+tax_equi_Ipca)*(1+taxa_equivalente_Ipca))
+        VF = (VF + montante) * ((1+taxa_equi_Ipca)*(1+taxa_equivalente_Ipca))
     return VF
-teste5 = invest_Ipca_mais((300*12/252), 1260, 5.5)
 
-print(f"+IPCA: {teste5}")
+def calcular_e_listar():
+    montante = float(input("Digite o montante do investimento: "))
+    tempo = int(input("Digite o período de tempo do investimento (em dias): "))
+    taxa = float(input("Digite a taxa de juros (%): "))
+
+    resultados = {
+        "SELIC": invest_selic(montante, tempo, taxa),
+        "CDI": invest_cdi(montante, tempo, taxa),
+        "IPCA": invest_ipca(montante, tempo, taxa),
+        "Porcentagem %CDI": invest_porcentagem_cdi(montante, tempo, taxa, 110),
+        "+IPCA": invest_Ipca_mais(montante, tempo, taxa, 5.5)
+    }
+
+    # Ordenar os resultados do melhor ao pior
+    resultados_ordenados = sorted(resultados.items(), key=lambda x: x[1], reverse=True)
+
+    print("Lista de investimentos do melhor ao pior:")
+    for investimento, valor in resultados_ordenados:
+        print(f"{investimento}: {valor}")
+
+calcular_e_listar()
